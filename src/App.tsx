23325a001fab5ad079cc8923694e8171ad6e7cd0
Aleksei-89ST <leha.leonov.52@bk.ru>
2023-02-06 22:46:20 +0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const buttonClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isActive ? "container active" : "container"}>
+      <div className="block">
+        <section className="block-item block__item">
+          <h2 className="block-item__title">У вас уже есть аккаунт?</h2>
+          <button onClick={buttonClick} className="block-item__btn signin-btn">
+            Вход
+          </button>
+        </section>
+
+        <section className="block-item block__item">
+          <h2 className="block-item__title">У вас нет аккаунта?</h2>
+          <button onClick={buttonClick} className="block-item__btn signup-btn">
+            Зарегистрироваться
+          </button>
+        </section>
+      </div>
+      <div className={isActive ? "form-box active" : "form-box"}>
+        <form action="#" className="form form_signin">
+          <h3 className="form__title">Вход</h3>
+          <p>
+            <input type="text" className="form__input" placeholder="Логин" />
+          </p>
+          <p>
+            <input
+              type="password"
+              className="form__input"
+              placeholder="Пароль"
+            />
+          </p>
+          <p>
+            <button className="form__btn">Войти</button>
+          </p>
+          <p>
+            <Link to="#" className="form__forgot">
+              Восстановить пароль
+            </Link>
+          </p>
+        </form>
+
+        <form action="#" className="form form_signup">
+          <h3 className="form__title">Регистрация</h3>
+          <p>
+            <input type="text" className="form__input" placeholder="Логин" />
+          </p>
+          <p>
+            <input type="email" className="form__input" placeholder="Email" />
+          </p>
+          <p>
+            <input
+              type="password"
+              className="form__input"
+              placeholder="Пароль"
+            />
+          </p>
+          <p>
+            <input
+              type="password"
+              className="form__input"
+              placeholder="Подтвердить пароль"
+            />
+          </p>
+          <p>
+            <button className="form__btn form__btn_signup">
+              Зарегистрироваться
+            </button>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
